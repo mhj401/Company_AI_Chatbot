d73d5d15d5fbee0d5ecfe4a2a37d4e2bc446470f
moonhj <mhj401@gmail.com>
2026-03-06 15:01:45 +0900

    feat: update chatbot responses and improve server configuration

--- server.py
+++ server.py
@@ -5,17 +5,15 @@ app = Flask(__name__)
 
 # --- [설정] 회사 URL (여기에 시연할 URL 입력) ---
 TARGET_URLS = [
-    "https://platformoz.com/",         
-    "https://platformoz.com/company",  
-    "https://platformoz.com/heradee",
-    "https://platformoz.com/aixoz",
-    "https://platformoz.com/chmo"
+    "https://samjungenr.imweb.me/main",
+    "https://samjungenr.imweb.me/aboutus",
+    "https://samjungenr.imweb.me/resource"
 ]
 
-print("🤖 챗봇 로딩 중... (잠시만 기다려주세요)")
-# 서버 켤 때 챗봇을 미리 한 번만 로드합니다.
+print("🤖 AI 로딩 중... (잠시만 기다려주세요)")
+# 서버 켤 때 AI을 미리 한 번만 로드합니다.
 bot = CompanyChatbot(urls=TARGET_URLS)
-print("✅ 챗봇 준비 완료!")
+print("✅ AI 준비 완료!")
 
 @app.route('/')
 def home():
@@ -27,7 +25,7 @@ def ask():
     # 화면에서 보낸 질문 받기
     user_input = request.json.get('msg')
     
-    # 챗봇에게 물어보기
+    # AI에게 물어보기
     response = bot.ask(user_input)
     
     # 결과가 딕셔너리({result: ...}) 형태면 텍스트만 추출
@@ -37,4 +35,5 @@ def ask():
     return jsonify({'answer': answer_text})
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    app.run(debug=False, port=5000)
